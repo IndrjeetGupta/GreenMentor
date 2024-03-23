a99fef1236b52db95ed480353509afb2cf087abd
IndrjeetGupta <indrajeetshanu1@gmail.com>
2024-03-23 11:36:10 +0530

    greenMentorBeckApi

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # GreenMentor
+
+Authentication
+
+user Details
+
+Register
+
+Login
+
+POST - /api/register
+
+POST - /api/login
+
+GET - /api/tasks
+
+GET - /api/tasks/:id
+
+POST - /api/tasks
+
+PUT / PATCH - /api/tasks/:id
+
+DELETE - /api/tasks/:id
---
+++ routes/user.routes.js
@@ -0,0 +1,47 @@
+const { Router } = require("express")
+const { UserModel } = require('../models/User.model')
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const dotenv = require("dotenv")
+const useController = Router();
+
+useController.post("/register", (req, res) => {
+    const { name, email, password } = req.body
+
+    bcrypt.hash(password, 5, async function (err, hash) {
+        if (err) {
+            res.send("something went wrong , plz try agsin later")
+        }
+        const user = new UserModel({
+            name,
+            email,
+            password: hash,
+        })
+        await user.save()
+        res.json({ message: "Signup successful" })
+    });
+
+})
+
+useController.post("/login", async (req, res) => {
+    const { email, password } = req.body
+    const user = await UserModel.findOne({ email })
+    const hash = user.password
+    bcrypt.compare(password, hash, function (err, result) {
+        if (err) {
+            res.send("Something went wrong , plz try again")
+        }
+        if (result) {
+            const token = jwt.sign({ userId: user._id }, process.env.secretCode);
+            res.json({ message: "Login successfull", token })
+        }
+        else {
+            res.send("invalid credential , plz signup ")
+        }
+    });
+
+})
+
+module.exports = {
+    useController
+}
